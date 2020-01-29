7dd957f5284d6129967156e88bb02bd481f7880e
Ajiboye Eniola <eniolaa@opay.team>
2020-01-29 17:27:59 +0100

    Feat: Add wellvis icon and shadows

--- src/js/wellvis-widget-1.0.js
+++ src/js/wellvis-widget-1.0.js
@@ -1,9 +1,15 @@
-!function() {
+!function(widgetId) {
+
+  let widgetFrame = null;
+
   const isDesktop = window.matchMedia("(min-width: 768px)").matches;
   let btn, overlay, iframe;
   
-  const buildCloseBtn = function(cb) {
+  const buildCloseBtn = (cb) => {
     const btn = document.createElement("button");
+    btn.lineHeight = "41px"
+    btn.height = "41px"
+    btn.paddingBottom = "4px"
     btn.textContent = "\u00D7";
     btn.style.border = "none";
     btn.style.outline = "none";
@@ -16,16 +22,24 @@
     btn.style.left = "-20px"; 
     btn.style.fontWeight = 100;
     btn.style.fontSize = "2rem";
+    btn.style.paddingLeft = "11px";
+    btn.style.paddingRight = "11px";
+    btn.style.cursor = "pointer";
     btn.addEventListener("pointerdown", (e) => {
       cb && cb(e)
-      overlay.remove();
+      overlay.style.height = "0px";
     })
     return btn
   }
   
-  const buildFrame = function() {
-    frame = document.createElement("iframe");
+  const buildFrame = () => {
 
+    if(widgetFrame){
+      return widgetFrame;
+    } 
+
+    frame = document.createElement("iframe");
+    
     frame.frameBorder = "0";
     frame.style.top = "0";
     frame.style.left = "0";
@@ -35,18 +49,35 @@
     frame.style.width = "100%";
     frame.style.height = "100%";
     frame.style.position = "absolute";
-    frame.src = "https://staging.wellvis.org/#/chat/home"
+    frame.src = `https://staging.wellvis.org/#/chat/find/widget/${widgetId}`;
 
+    widgetFrame = frame;
     return frame;
   };
 
-  const buildOverlay = function(frame) {
+  const buildNavbar = () => {
+    navbar = document.createElement("div");
+
+    navbar.style.display= "flex";
+    navbar.style.justifyContent= "space-between";
+    navbar.style.boxShadow = "0px 4px 6px 0px rgba(0,0,0,0.2)";
+    navbar.style.height = "48px";
+    navbar.style.width = "100%";
+    navbar.style.background = "#EEEEEE";
+
+    return navbar;
+  }
+
+  const buildOverlay = (frame) => {
     overlay = document.createElement("div");
+
     overlay.className = "opay-overlay";
     overlay.style.overflow = "visible";
     overlay.style.zIndex = "999";
     overlay.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
     overlay.style.position = "absolute";
+    overlay.style.border = "2px solid #CCC";
+    overlay.style.boxShadow = "-4px -2px 12px 0px rgba(0,0,0,0.2)";
     overlay.style.width = "100%";
     overlay.style.height = "100%";
 
@@ -66,46 +97,52 @@
     return overlay;
   };
   
-  const buildButton = function(cb) {
+  const buildButton = (cb) => {
     const btn = document.createElement("button");
     btn.textContent = "W"
+    btn.lineHeight = "41px"
+    btn.height = "41px"
     btn.style.fontFamily = "cursive";
     btn.style.border = "none";
     btn.style.outline = "none";
     btn.style.borderRadius = "50%";
     btn.style.padding = "15px 20px";
     btn.style.boxShadow = "0px 0px 12px 0px rgba(0,0,0,0.2)";
-    btn.style.color = "white";
-    btn.style.backgroundColor = "rgb(201, 140, 49)";
+    btn.style.color = "transparent";
+    btn.style.backgroundColor = "transparent";
+    btn.style.background = "url('src/img/icon.png') no-repeat";
+    btn.style.backgroundPosition = "center";
+    btn.style.backgroundSize = "contain";
+    btn.style.cursor = "pointer";
     btn.style.position = "fixed";
     btn.style.bottom = "70px";
     btn.style.right = "40px"; 
-    btn.style.fontWeight = 800; 
-    btn.style.fontSize = "1.3rem";
     btn.addEventListener("pointerdown", (e) => {
       cb(e)
       btn.remove();
     })
     return btn
   }
+
+  const showButton = () => {
+    btn = buildButton(showFrame)
+    document.body.appendChild(btn)
+  }
   
-  const showFrame = function() {
+  const showFrame = () => {
     iframe = buildFrame();
     overlay = buildOverlay(iframe);
     const closeBtn = buildCloseBtn(function () {
       closeBtn.remove();
+      showButton();
     })
     overlay.appendChild(closeBtn)
     document.body.appendChild(overlay)
   }
   
- window.Wellvis = {
-   open() {
-    showFrame()
-   },
-   showButton() {
-     btn = buildButton(showFrame)
-     document.body.appendChild(btn)
-   }
- } 
-}()
+  window.Wellvis = {
+    showButton,
+    open: showFrame
+    
+  } 
+}('5c4ff300-d79a-11e9-8a36-3f1902cff575')
